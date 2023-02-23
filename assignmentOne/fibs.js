c5ff0610f477402db85d7825f3b479692e585a00
Daniele Campari <danielecampari@gmail.com>
2023-02-23 11:59:54 +0100

    Add iterative fibonacci

---
+++ assignmentOne/fibs.js
@@ -0,0 +1,29 @@
+/**
+ * Using iteration, write a function fibs which takes a number and returns
+ * an array containing that many numbers from the Fibonacci sequence.
+ * Using an example input of 8, this method should return the array
+ * [0, 1, 1, 2, 3, 5, 8, 13].
+ * @param {Number} n - Integer
+ * @returns {Array} Fibonacci Sequence of that number
+ */
+function fibs(n) {
+  const sequence = [];
+  let a = 0;
+  let b = 1;
+
+  if (n >= 0) sequence.push(a);
+
+  if (n >= 1) sequence.push(b);
+
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    sequence.push(c);
+
+    a = b;
+    b = c;
+  }
+  return sequence;
+}
+
+const test = fibs(8);
+console.log(test);
